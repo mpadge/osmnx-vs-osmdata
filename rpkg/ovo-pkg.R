6bbbaa9abd4bb02d72d53550e83b5be4a456511e
mpadge <mark.padgham@email.com>
2019-03-04 20:54:22 +0100

    add rpkg

---
+++ rpkg/ovo-pkg.R
@@ -0,0 +1,15 @@
+#' rdeps
+#'
+#' Automatically obtain source code for all dependencies of a given R package,
+#' either by opening github sites in browser, or downloading .tar.gz files.
+#'
+#' @section Functions:
+#' \tabular{ll}{
+#' \code{get_rdeps}\tab Obtain source code
+#' }
+#'
+#' @name rdeps
+#' @docType package
+#' @importFrom XML readHTMLTable
+#' @importFrom utils available.packages browseURL download.file
+NULL
